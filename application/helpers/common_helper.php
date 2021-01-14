57ac42fcbefedef8cb8b5c230a18b4409bf3a127
shiva prasad <shiva@secondsdigital.com>
2021-01-14 17:27:22 +0530

    SMTP setup

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -20,16 +20,15 @@ if ( ! function_exists('email_config'))
         // $config['wordwrap'] = TRUE;
         // $config['mailtype'] = 'html';
         $config['protocol']='smtp';
-$config['smtp_host']='ssl://smtp.gmail.com';
-$config['smtp_port']='465';
-$config['smtp_timeout']='30';
-$config['smtp_user']='pintu19mg@gmail.com';
-$config['smtp_pass']='Password1982';
-$config['charset']='utf-8';
-$config['newline']="\r\n";
-$config['wordwrap'] = TRUE;
-$config['mailtype'] = 'html';
-
+        $config['smtp_host']='ssl://smtp.gmail.com';
+        $config['smtp_port']='465';
+        $config['smtp_timeout']='30';
+        $config['smtp_user']='crmcalendar05@gmail.com';
+        $config['smtp_pass']='9866115824';
+        $config['charset']='utf-8';
+        $config['newline']="\r\n";
+        $config['wordwrap'] = TRUE;
+        $config['mailtype'] = 'html';
         return $config;
     }   
 }
